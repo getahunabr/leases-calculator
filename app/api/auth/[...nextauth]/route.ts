9d060be31696a99ae865945435968332ed4f3f4a
getahunabr <147873183+getahunabr@users.noreply.github.com>
2025-01-10 10:52:06 -0800

    edited

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -45,7 +45,6 @@ const handler = NextAuth({
 
           return { id: user.id, email: user.email, name: user.name };
         } catch (error) {
-          console.error("Error during authorization:", error.message);
           throw new Error("Authentication failed");
         }
       },
