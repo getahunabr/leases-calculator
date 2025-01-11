d23dd731599adb1647feb8204fa8fa7d9261b9bf
getahunabr <147873183+getahunabr@users.noreply.github.com>
2025-01-11 01:51:57 -0800

    edit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,36 @@
-import { PrismaClient } from "@prisma/client";
-const Prisma = new PrismaClient();
+// import { PrismaClient } from "@prisma/client";
+// const Prisma = new PrismaClient();
 
-async function main() {
-  const user1 = await prisma.user.create({
-    data: { email: "owner@example.com", name: "owner" },
-  });
-  const user2 = await prisma.user.create({
-    data: { email: "invitee@example.com", name: "Invitee" },
-  });
-  await prisma.lease.create({
-    data: {
-      startDate: new Date(),
-      endDate: new Date(),
-      monthlyRent: 1000,
-      ownerId: user1.id,
-    },
-  });
-  main()
-    .catch((e) => console.error(e))
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
-}
+// async function main() {
+//   const user1 = await Prisma.user.create({
+//     data: {
+//       email: "owner@example.com",
+//       name: "owner",
+//       password: "securepassword",
+//     },
+//   });
+//   const user2 = await Prisma.user.create({
+//     data: {
+//       email: "invitee@example.com",
+//       name: "Invitee",
+//       password: "securepassword",
+//     },
+//   });
+//   await Prisma.lease.create({
+//     data: {
+//       leaseStartDate: new Date(),
+//       leaseEndDate: new Date(
+//         new Date().setFullYear(new Date().getFullYear() + 1)
+//       ),
+//       monthlyRent: 1000,
+//       securityDeposit: 500,
+//       ownerId: user1.id,
+//       leaseType: "RESIDENTIAL",
+//     },
+//   });
+//   main()
+//     .catch((e) => console.error(e))
+//     .finally(async () => {
+//       await Prisma.$disconnect();
+//     });
+// }
